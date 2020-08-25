ff890e15a1cc3debbb5314cc8d46de669bff4555
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-25 23:51:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -71,65 +71,65 @@ def adding_matz
 #   :languages => ["LISP", "C"]
 # }
 # return the entire updated hash
-
-	programmer_hash =
- 		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
-      }
-    }
-
-end
-
-def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable.
-  # return the entire updated hash
-
-	programmer_hash =
- 		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
-      }
-    }
-
-end
-
-def adding_to_dennis
-  # add "Assembly" to Dennis Ritchie's languages array
-  # return the entire updated hash
-
-	programmer_hash =
- 		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
-      }
-    }
-
-end
+# 
+# 	programmer_hash =
+#  		{
+#       :grace_hopper => {
+#         :known_for => "COBOL",
+#         :languages => ["COBOL", "FORTRAN"]
+#       },
+#       :alan_kay => {
+#         :known_for => "Object Orientation",
+#         :languages => ["Smalltalk", "LISP"]
+#       },
+#       :dennis_ritchie => {
+#         :known_for => "Unix",
+#         :languages => ["C"]
+#       }
+#     }
+# 
+# end
+# 
+# def changing_alan
+#   # change what Alan Kay is :known_for to the value of the alans_new_info variable.
+#   # return the entire updated hash
+# 
+# 	programmer_hash =
+#  		{
+#       :grace_hopper => {
+#         :known_for => "COBOL",
+#         :languages => ["COBOL", "FORTRAN"]
+#       },
+#       :alan_kay => {
+#         :known_for => "Object Orientation",
+#         :languages => ["Smalltalk", "LISP"]
+#       },
+#       :dennis_ritchie => {
+#         :known_for => "Unix",
+#         :languages => ["C"]
+#       }
+#     }
+# 
+# end
+# 
+# def adding_to_dennis
+#   # add "Assembly" to Dennis Ritchie's languages array
+#   # return the entire updated hash
+# 
+# 	programmer_hash =
+#  		{
+#       :grace_hopper => {
+#         :known_for => "COBOL",
+#         :languages => ["COBOL", "FORTRAN"]
+#       },
+#       :alan_kay => {
+#         :known_for => "Object Orientation",
+#         :languages => ["Smalltalk", "LISP"]
+#       },
+#       :dennis_ritchie => {
+#         :known_for => "Unix",
+#         :languages => ["C"]
+#       }
+#     }
+# 
+# end
