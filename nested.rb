3fa36e830a23c1d6f97988178a0d73a66e4f042d
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-25 23:51:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -71,7 +71,7 @@ def adding_matz
 #   :languages => ["LISP", "C"]
 # }
 # return the entire updated hash
-# 
+#
 # 	programmer_hash =
 #  		{
 #       :grace_hopper => {
@@ -87,13 +87,13 @@ def adding_matz
 #         :languages => ["C"]
 #       }
 #     }
-# 
+#
 # end
-# 
+#
 # def changing_alan
 #   # change what Alan Kay is :known_for to the value of the alans_new_info variable.
 #   # return the entire updated hash
-# 
+#
 # 	programmer_hash =
 #  		{
 #       :grace_hopper => {
@@ -109,13 +109,13 @@ def adding_matz
 #         :languages => ["C"]
 #       }
 #     }
-# 
+#
 # end
-# 
+#
 # def adding_to_dennis
 #   # add "Assembly" to Dennis Ritchie's languages array
 #   # return the entire updated hash
-# 
+#
 # 	programmer_hash =
 #  		{
 #       :grace_hopper => {
@@ -131,5 +131,5 @@ def adding_matz
 #         :languages => ["C"]
 #       }
 #     }
-# 
+#
 # end
