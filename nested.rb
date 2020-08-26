ffd8b80f700ed13101f783f1215cb4e250a05821
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-26 00:07:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,11 +59,6 @@ programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
-   programmer_hash[:yukihiro_matsumoto] = {
-      :known_for => "Ruby",
-      :languages => ["LISP", "C"]
-    }
-
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
@@ -86,6 +81,10 @@ def adding_matz
         :languages => ["C"]
       }
     }
+programmer_hash[:yukihiro_matsumoto] = {
+       :known_for => "Ruby",
+       :languages => ["LISP", "C"]
+     }
 programmer_hash
 end
 #
