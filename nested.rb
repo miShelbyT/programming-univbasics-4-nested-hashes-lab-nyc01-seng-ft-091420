d09373a0623e5728c6b2cca88204346dbb0e7620
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-26 00:14:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,6 @@ programmer_hash[:yukihiro_matsumoto] = {
        :known_for => "Ruby",
        :languages => ["LISP", "C"]
      }
-programmer_hash[:alan_kay][:known_for] = "GUI"
 programmer_hash
 end
 #
@@ -108,7 +107,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash
 end
 
 def adding_to_dennis
